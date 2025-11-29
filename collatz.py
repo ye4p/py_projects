d6b0f7dfa7c208aea9f95b92b035d131f93504f1
Danila Golubkov <danilgolubkov172@gmail.com>
2025-11-29 17:28:13 -0500

    Now shows biggest number of steps in sequence

--- collatz.py
+++ collatz.py
@@ -1,12 +1,19 @@
 def main(min, max):
+    maxSteps=0
+    maxX=0
     for x in range(min, max):
         print(x, " is a number x")
         n=x
+        count=0
         while n!=1:
+            count+=1
             if n %2==0:
                 n=n/2
             else:
                 n=n*3+1
-        print(f"Number {x} got to zero")
-    return True
-main(2, 100000000)
+        print(f"Number {x} got to zero in {count} steps")
+        if count>maxSteps:
+            maxSteps=count
+            maxX=x
+    print(f"Biggest num of steps: {maxSteps} for x: {maxX}")
+main(2, 1000000)
